77aa760aa79ad8298c3d3daa71fdda0eab27ffc1
Aryan07982 <aryangupta51005@user.noreply.github.com>
2025-08-17 19:24:40 +0530

    added game logic and functionality

--- script.js
+++ script.js
@@ -1,62 +1,98 @@
+let humanScore = 0;
+let computerScore = 0;
+let roundNum = 1;
+
+const choiceButtons = document.querySelector(".choice-buttons");
+const choices = document.querySelectorAll('.choices');
+const result = document.querySelector('.result');
+const disHumanScore = document.querySelector('.human-score');
+const disComputerScore = document.querySelector('.computer-score');
+const round = document.querySelector('.round');
+const nxtRoundBtn = document.querySelector('#nxtrnd');
+const playAgainBtn = document.querySelector('#pagn');
+
+nxtRoundBtn.disabled = true;
+playAgainBtn.disabled = true;
+
 function getComputerChoice(){
     let choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random()*3)];
 }
 
-function getHumanChoice(){
-    let humanChoice =  prompt("CHOOSE ROCK, PAPER OR SCISSORS: ");
-    return humanChoice.toLowerCase();
-}
-
 function playRound(humanChoice, computerChoice){
     if (humanChoice === computerChoice)
         {
-            humanScore += 1;
-            computerScore += 1;
-            console.log(`DRAW! YOU BOTH CHOSE ${humanChoice}!
-YOUR SCORE: ${humanScore}  COMPUTER SCORE: ${computerScore}`);
-            // console.log(`YOUR SCORE: ${humanScore}  COMPUTER SCORE: ${computerScore}`);
+            result.textContent = `DRAW! YOU BOTH CHOSE ${humanChoice}!`;
     } else if (
             (humanChoice === 'rock' && computerChoice === 'paper') ||
             (humanChoice === 'paper' && computerChoice === 'scissors') ||
-            (humanChoice === 'scissor' && computerChoice === 'rock')
+            (humanChoice === 'scissors' && computerChoice === 'rock')
         )
         {
-            computerScore += 2;
-            console.log(`YOU LOSE! ${computerChoice} BEATS ${humanChoice}
-YOUR SCORE: ${humanScore}  COMPUTER SCORE: ${computerScore}`);
-            // console.log(`YOUR SCORE: ${humanScore}  COMPUTER SCORE: ${computerScore}`);
+            computerScore += 1;
+            disComputerScore.textContent = computerScore;
+            result.textContent = `YOU LOSE! ${computerChoice} BEATS ${humanChoice}`;
         }
         else{
-            humanScore += 2;
-            console.log(`YOU WIN! ${humanChoice} BEATS ${computerChoice}
-YOUR SCORE: ${humanScore}  COMPUTER SCORE: ${computerScore}`);
-            // console.log(`YOUR SCORE: ${humanScore}  COMPUTER SCORE: ${computerScore}`);
+            humanScore += 1;
+            disHumanScore.textContent = humanScore;
+            result.textContent = `YOU WIN! ${humanChoice} BEATS ${computerChoice}`;
         }
-    
+
+    if (computerScore === 5 || humanScore === 5){
+        declareWinner();
+        playAgainBtn.disabled = false;
+        return;
+    }
+    nxtRoundBtn.disabled = false;
 }
 
-// function playGame(playRound){
+function declareWinner(){
+    if(humanScore===5){
+        result.textContent = `CONGRATULATIONS! YOU WON THE GAME BY ${humanScore - computerScore} POINTS!`;
+    } else if(computerScore===5){
+        result.textContent = `YOU LOST! COMPUTER BEAT YOU BY ${computerScore - humanScore} POINTS!`;
+    }
+}
 
-//     for(let i = 0; i < 5; i++){
-//         console.log(`ROUND ${i+1}: `);
-//         let humanSelection = getHumanChoice();
-//         let computerSelection = getComputerChoice();
-//         playRound(humanSelection, computerSelection);
-//     }
-    
-//     if (humanScore > computerScore){
-//         console.log('CONGRATULATIONS! YOU WON THE GAME!!!');
-//         console.log(`YOU BEAT THE COMPUTER BY ${humanScore - computerScore} POINTS!`);
-//     } else if (humanScore == computerScore){
-//         console.log('WELL PLAYED! IT IS A DRAW');
-//     } else{
-//         console.log('YOU LOST! BETTER LUCK NEXT TIME');
-//         console.log(`COMPUTER BEAT YOU BY ${computerScore - humanScore} POINTS!`);
-//     }
-// }
+choiceButtons.addEventListener('click', (e)=>{
+    choices.forEach(btn => btn.disabled = true);
+    let target = e.target;
+    let humanChoice;
+    switch (target.id){
+        case 'rockBtn':
+            humanChoice = 'rock';
+            break;
+        case 'paperBtn':
+            humanChoice = 'paper';
+            break;
+        case 'scissorBtn':
+            humanChoice = 'scissors'
+            break;
+    }
+    let computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+})
 
-let humanScore = 0;
-let computerScore = 0;
+nxtRoundBtn.addEventListener('click', ()=>{
+    choices.forEach(btn => btn.disabled = false);
+    roundNum += 1;
+    round.textContent = roundNum;
+    nxtRoundBtn.disabled = true;
+})
 
-// playGame(playRound);
+playAgainBtn.addEventListener('click', ()=>{
+    humanScore = 0;
+    computerScore = 0;
+
+    disComputerScore.textContent = computerScore;
+    disHumanScore.textContent = humanScore;
+
+    roundNum = 1;
+    round.textContent = roundNum
+
+    result.textContent = 'BEST OF LUCK!';
+
+    choices.forEach(btn=> btn.disabled = false);
+    playAgainBtn.disabled = true;
+})
